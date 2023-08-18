4fe783505f668d7e77f43c2c580c0835f05dc9a1
Amandrews <amandrews09@gmail.com>
2023-08-18 10:15:01 -0600

    stopping timer

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,22 @@
+var interval
+
 function startQuiz(){
     console.log("Hello") 
     //setting interval for how fast timer counts down
-setInterval(timer, 1000)
+interval = setInterval(timer, 1000);
 
 }
-//timer, counting down from 60
-var count = 59
+//timer, counting down from 10
+var count = 9
 function timer() {
   var timer= document.getElementById("time")
   timer.textContent = count;
   count = count-1;
+  if(count < 0) {
+    console.log("stop timer")
+    clearInterval(interval)
+
+  }
 }
 //start button listening for click
 var startBtn = document.getElementById("startBtn")
